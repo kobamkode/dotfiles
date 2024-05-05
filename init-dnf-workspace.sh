b0fe218dea99136abef1054efa307b3d0e33415d
Mario <142477579+kobamkode@users.noreply.github.com>
2024-05-05 20:39:44 +0700

    Create init-dnf-workspace.sh

---
+++ init-dnf-workspace.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/bash
+set -ex
+echo "Update packages"
+sudo dnf update -y
+
+echo "Upgrade packages"
+sudo dnf upgrade -y
+
+echo "Install Neovim"
+sudo dnf install neovim ripgrep fd-find -y
+
+echo "Install build-essential"
+sudo dnf group install "C Development Tools and Libraries" -y
+
+echo "Install Github CLI"
+sudo dnf install gh -y
+
+echo "Install GitUI"
+sudo dnf install gitui -y
+
+echo "Install Stow"
+sudo dnf install stow -y
+
+echo "Install Zoxide"
+sudo dnf copr enable atim/zoxide -y
+sudo dnf install zoxide
+
+echo "Install Tmux"
+sudo dnf install tmux -y
+
+echo "Clone TPM"
+if [ ! -d "~/.tmux/plugins/tpm"]; then
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
