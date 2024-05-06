7d9c649ab0a458f0454efde6e311944db719133b
Mario <142477579+kobamkode@users.noreply.github.com>
2024-05-06 14:41:38 +0700

    Update init-dnf-workspace.sh

--- init-dnf-workspace.sh
+++ init-dnf-workspace.sh
@@ -25,29 +25,34 @@ sudo dnf install stow -y
 echo "Install Zoxide"
 sudo dnf copr enable atim/zoxide -y
 sudo dnf install zoxide -y
+echo eval "$(zoxide init bash)" >> "$HOME/.bashrc"
 
 echo "Install Tmux"
 sudo dnf install tmux -y
 
 echo "Clone TPM"
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 fi
 
 echo "Swap Capslock into Esc"
 sudo dnf install dconf -y
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 
+echo "Install Go"
+curl -O https://www.digitalocean.com/robots.txt
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
+echo export PATH=$PATH:/usr/local/go/bin >> "$HOME/.bashrc"
+
 echo "Append configs into .bashrc"
-echo -e eval "$(zoxide init bash)" >> $HOME/.bashrc
-echo -e alias ll="ls -al --color" >> $HOME/.bashrc
+echo alias ll="ls -al --color" >> "$HOME/.bashrc"
+source "$HOME/.bashrc"
 
 echo "Stow dotfiles"
-if [ "$PWD" != "$HOME/dotfiles" ]; then
-cd "$HOME/dotfiles" || exit
+# Change directory to $HOME/dotfiles if not already there
+[[ "$PWD" != "$HOME/dotfiles" ]] && cd "$HOME/dotfiles" || :
+# Run stow tasks
 stow nvim
 stow tmux
 stow .fonts
 stow gitui
-fi
-
