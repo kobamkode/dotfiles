6b8f47ff108784950d94434f550d2d268805a601
Mario <142477579+kobamkode@users.noreply.github.com>
2024-05-06 17:52:45 +0700

    Update init-dnf-workspace.sh

--- init-dnf-workspace.sh
+++ init-dnf-workspace.sh
@@ -25,7 +25,7 @@ sudo dnf install stow -y
 echo "Install Zoxide"
 sudo dnf copr enable atim/zoxide -y
 sudo dnf install zoxide -y
-echo eval "$(zoxide init bash)" >> "$HOME/.bashrc"
+echo "eval '\$(zoxide init bash)'" >> "$HOME/.bashrc"
 
 echo "Install Tmux"
 sudo dnf install tmux -y
@@ -41,12 +41,13 @@ dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 
 echo "Install Go"
 curl -O https://dl.google.com/go/go1.22.2.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
-echo export PATH=$PATH:/usr/local/go/bin >> "$HOME/.bashrc"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
+echo "export PATH=\$PATH:/usr/local/go/bin" >> "$HOME/.bashrc"
 sudo rm -rf go1.22.2.linux-amd64.tar.gz
 
 echo "Append configs into .bashrc"
-echo alias ll="ls -al --color" >> "$HOME/.bashrc"
+echo "alias ll='ls -al'" >> "$HOME/.bashrc"
 source "$HOME/.bashrc"
 
 echo "Stow dotfiles"
