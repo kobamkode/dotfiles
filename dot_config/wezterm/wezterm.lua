eaa0d016bb26cafed95cf55b683b3d27e9812ca1
Mario Wibisono <kobamkode@gmail.com>
2024-09-21 13:00:40 +0700

    Update .config/i3/config Update .config/nvim/lazy-lock.json Update .config/nvim/lua/kickstart/plugins/cmp.lua Update .config/nvim/lua/lazy-plugins.lua Update .config/nvim/lua/options.lua Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
 local config = {}
 
--- config.color_scheme = "Batman"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrains Mono")
-config.font_size = 10.0
-config.line_height = 1.2
+config.font_size = 10.7
+config.line_height = 1.0
 config.hide_tab_bar_if_only_one_tab = true
 config.prefer_to_spawn_tabs = true
 
