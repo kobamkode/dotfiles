27057f19e0b386fb2ae3662201c5cf15af02eb24
Mario Wibisono <kobamkode@gmail.com>
2024-10-17 20:09:02 +0700

    update config

--- dotfiles/executable_bootstrap.sh
+++ dotfiles/executable_bootstrap.sh
@@ -21,8 +21,24 @@ post_install() {
 		flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 	fi
 
+	if [[ ! -e "/home/mario/.cargo/bin/cargo" ]]; then
+		rustup-init -y
+		source "$HOME/.cargo/env.fish"
+	fi
 
-	if [[ -e "/usr/bin/evremap" ]] && [[ -e "/usr/lib/systemd/system/evremap.service" ]]; then
+	if [[ ! -e "/usr/bin/evremap" ]]; then
+		git clone https://github.com/wez/evremap.git
+		cd evremap
+		cargo build --release
+		sudo cp target/release/evremap /usr/bin/evremap
+		sudo cp /home/mario/dotfiles/logitech-wave-keys.toml /etc/evremap.toml
+		sudo cp evremap.service /usr/lib/systemd/system/
+		sudo systemctl daemon-reload
+		sudo systemctl enable evremap.service
+		sudo systemctl start evremap.service
+	fi
+
+	if [[ -e "/usr/lib/systemd/system/evremap.service" ]]; then
 		rm -rf $HOME/evremap $HOME/README.md
 	fi
 
@@ -37,33 +53,20 @@ install_pkgs() {
 		fi
 
 		if [[ -z $v ]]; then
+			echo "=============================="
 			echo "Installing $i..."
+			echo "=============================="
 			if [[ $OS == "fedora" ]]; then
 				if [[ $i == "wezterm" ]]; then
 					sudo dnf copr enable wezfurlong/$i-nightly -y
 				fi
 				
-				# if [[ $i == "starship" ]] || [[ $i == "zoxide" ]]; then
-				# 	sudo dnf copr enable atim/$i -y
-				# fi
-
 				if [[ $i == "nerd-fonts" ]]; then
 					sudo dnf copr enable che/$i -y
 				fi
 
 				if [[ $i == "evremap" ]]; then
 					sudo dnf install -y libevdev-devel
-					git clone https://github.com/wez/evremap.git
-					cd evremap
-					rustup-init -y
-					source "$HOME/.cargo/env.fish"
-					cargo build --release
-					sudo cp target/release/evremap /usr/bin/evremap
-					sudo cp /home/mario/dotfiles/logitech-wave-keys.toml /etc/evremap.toml
-					sudo cp evremap.service /usr/lib/systemd/system/
-					sudo systemctl daemon-reload
-					sudo systemctl enable evremap.service
-					sudo systemctl start evremap.service
 					continue
 				fi
 
