7bf0976f5d91e26daa7078ca3de478a2b44aabdf
Mario Wibisono <kobamkode@gmail.com>
2024-11-03 15:54:26 +0700

    update fish and nvim option

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 set fish_greeting
-set -a fish_user_paths /usr/local/go/bin
+set -a fish_user_paths /usr/local/go/bin /home/mario/go/bin
 
 alias ls="ls -al --color"
 alias fisource="source ~/.config/fish/config.fish"
