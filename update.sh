ccdbaf870695907adc27335a778ca9f2315e17af
Mario Wibisono <kobamkode@gmail.com>
2026-04-17 11:13:02 +0700

    update dotfiles

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 gum style --background="3" --foreground="0" "  Updating mise  "
-mise up --bump
+mise up
 
 gum style --background="3" --foreground="0" "  Updating flatpak  "
 flatpak update -y
